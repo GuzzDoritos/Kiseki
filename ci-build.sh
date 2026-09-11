edb904cffe1beabf6733b77782506cbb2a169d4e
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-09-11 10:14:37 -0300

    build: update ci-build.sh to export DOTNET_ROOT and run dotnet publish

--- ci-build.sh
+++ ci-build.sh
@@ -5,4 +5,12 @@ DOTNET_VERSION=10.0
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c $DOTNET_VERSION -InstallDir ./dotnet
-./dotnet/dotnet --version
+
+# Export to PATH so 'dotnet' can be called
+export DOTNET_ROOT="$PWD/dotnet"
+export PATH="$DOTNET_ROOT:$PATH"
+
+dotnet --version
+
+# Publish Kiseki.Web into Release output folder
+dotnet publish Kiseki.Web/Kiseki.Web.csproj -c Release -o out
